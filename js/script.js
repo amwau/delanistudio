9047538af3a8e93aed6e069e07f5ed8cf31550a3
amwau <113367111+amwau@users.noreply.github.com>
2022-11-20 12:29:18 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
-(document).ready(function() {
+$(document).ready(function() {
 
-    ("#design").click(function(){
-      ("#design-showing").toggle();
-      (".hide-design-image").toggle();
+    $("#design").click(function(){
+        $("#design-showing").toggle();
+      $(".hide-design-image").toggle();
     })
   
-    ("#development").click(function(){
-      ("#development-showing").toggle();
-      (".hide-development-image").toggle();
+    $("#development").click(function(){
+        $("#development-showing").toggle();
+      $(".hide-development-image").toggle();
     })
   
-    ("#productManagement").click(function(){
-      ("#product-management-showing").toggle();
-      (".hide-product-management").toggle();
+    $("#productManagement").click(function(){
+      $("#product-management-showing").toggle();
+      $(".hide-product-management").toggle();
     })
   
     //Insert white box upo hover
-    (".Hover").hover(function(){
-      (this).animate({opacity:'1'});
+    $(".Hover").hover(function(){
+      $(this).animate({opacity:'1'});
     },
     function(){
       $(this).animate({opacity:'0'});
     })
-  ('.submit').click(function () {
+  $('.submit').click(function () {
           var Name = $('#mce-FNAME').val();
           var Email = $('#mce-EMAIL').val();
           var Message = $('#mce-MESSAGE');
